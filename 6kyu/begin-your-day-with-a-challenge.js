c51e7cd199274dbb9b236e6ba2de2fc4bf6fb0c8
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2022-11-05 01:50:46 -0300

    New Codewars challenge

--- 6kyu/begin-your-day-with-a-challenge.js
+++ 6kyu/begin-your-day-with-a-challenge.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/5873b2010565844b9100026d
+
 // There are no explanations. You have to create the code that gives the following results in Python, Ruby, and Haskell:
 
 // one_two_three(0) == [0, 0]
