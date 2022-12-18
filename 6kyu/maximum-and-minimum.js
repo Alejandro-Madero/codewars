ac041f781b2a81f13879203fc2cfe5d193a6e5d6
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2022-12-18 09:35:29 -0300

    New Codewars challenge

---
+++ 6kyu/maximum-and-minimum.js
@@ -0,0 +1,37 @@
+// https://www.codewars.com/kata/59c0cd4f46038781010000ac
+
+// Write two functions max and min which returns the maximum and minimum value of the argument passed into them respectively.
+
+// Example
+// max(1,2,3,4) //returns 4
+// max(1,2,3,['4']) //returns 4; note it returned 4 not '4'
+// max(1,2,[3,4]) //returns 4
+// max(1,2,[3,[4]]) //returns 4
+// max(1,2,[3,['4r']]) //returns NaN
+// max([[],[-4]]) // returns -4
+// max() or max([]) //returns 0
+// And so goes for min
+
+// Further Instructions
+// functions will take any number of arguments
+// Arrays of numbers can be to any depth
+// You can't use Math.max, Math.min, and require
+// If one of the arguments can not be evaluated to a number, return NaN
+
+const max = function () {
+  const args = Object.values(arguments)
+    .flat(Infinity)
+    .map((el) => +el);
+  if (!args.length) return 0;
+  args.sort((a, b) => b - a);
+  return args.includes(NaN) ? NaN : args[0];
+};
+
+const min = function () {
+  const args = Object.values(arguments)
+    .flat(Infinity)
+    .map((el) => +el);
+  if (!args.length) return 0;
+  args.sort((a, b) => a - b);
+  return args.includes(NaN) ? NaN : args[0];
+};
