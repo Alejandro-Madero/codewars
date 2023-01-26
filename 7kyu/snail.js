311b3b0d4116a5459bcdc82aeed8bb669bfc206e
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2023-01-26 20:15:18 -0300

    New Codewars challenge

---
+++ 7kyu/snail.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5b93fecd8463745630001d05/javascript
+
+// The snail crawls up the column. During the day it crawls up some distance. During the night she sleeps, so she slides down for some distance (less than crawls up during the day).
+
+// Your function takes three arguments:
+
+// The height of the column (meters)
+// The distance that the snail crawls during the day (meters)
+// The distance that the snail slides down during the night (meters)
+// Calculate number of day when the snail will reach the top of the column.
+
+const snail = function (column, day, night) {
+  if (column - day <= 0) return 1;
+  return 1 + snail(column - (day - night), day, night);
+};
