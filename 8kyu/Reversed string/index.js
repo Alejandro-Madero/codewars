f2463518810f4006469394df2bd6a3be11f9327a
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2023-02-08 23:54:05 -0300

    Add Codewars kata

---
+++ 8kyu/Reversed string/index.js
@@ -0,0 +1,15 @@
+/*
+
+LINK: https://www.codewars.com/kata/5168bb5dfe9a00b126000018/javascript
+
+DESCRIPTION:
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+*/
+
+// SOLUTION:
+
+const solution = (str) => str.split("").reverse().join("");
