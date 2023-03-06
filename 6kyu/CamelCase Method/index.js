54d9febc7892da95350ba149a53e52f222b004d2
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2023-03-05 21:03:23 -0300

    Added Codewars kata

---
+++ 6kyu/CamelCase Method/index.js
@@ -0,0 +1,27 @@
+/*
+
+LINK:
+https://www.codewars.com/kata/587731fda577b3d1b0001196/javascript
+
+DESCRIPTION:
+
+Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+For instance:
+
+"hello case".camelCase() => HelloCase
+"camel case word".camelCase() => CamelCaseWord
+
+*/
+
+// SOLUTION:
+
+String.prototype.camelCase = function () {
+  return this.split(" ")
+    .reduce((str, subStr, i) => {
+      if (!subStr) return [...str];
+      const camelStr = `${subStr[0].toUpperCase()}${subStr.slice(1)}`;
+      return [...str, camelStr];
+    }, [])
+    .join("");
+};
