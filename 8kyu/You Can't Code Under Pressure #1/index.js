cf1b0bf9a6149a3428c48033c96e6c091d3207d2
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2023-04-15 20:26:54 -0300

    Added Codewars kata

---
+++ 8kyu/You Can't Code Under Pressure #1/index.js
@@ -0,0 +1,14 @@
+/*
+
+LINK:
+https://www.codewars.com/kata/53ee5429ba190077850011d4/javascript
+
+DESCRIPTION: 
+
+Code as fast as you can! You need to double the integer and return it.
+
+*/
+
+//SOLUTION:
+
+const doubleInteger = (i) => i * 2;
