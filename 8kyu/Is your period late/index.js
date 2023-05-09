d3cd02252b4e7c2ac003826fce53ac5391b422bc
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2023-05-08 21:09:13 -0300

    Added Codewars kata

---
+++ 8kyu/Is your period late/index.js
@@ -0,0 +1,32 @@
+/*
+
+LINK:
+https://www.codewars.com/kata/578a8a01e9fd1549e50001f1/javascript
+
+DESCRIPTION: 
+
+In this kata, we will make a function to test whether a period is late.
+
+Our function will take three parameters:
+
+last - The Date object with the date of the last period
+
+today - The Date object with the date of the check
+
+cycleLength - Integer representing the length of the cycle in days
+
+Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false.
+
+
+
+const periodIsLate = function (last, today, cycleLength) {
+  return (today - last) / 86_400_000 > cycleLength;
+};
+
+*/
+
+// SOLUTION:
+
+const periodIsLate = function (last, today, cycleLength) {
+  return (today - last) / 86_400_000 > cycleLength;
+};
