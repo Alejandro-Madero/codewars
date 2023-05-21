dd815e6b39852148108c4e776563166b4461eaf9
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2023-05-21 20:43:29 -0300

    Added Codewars kata

---
+++ 8kyu/Calculate average/index.js
@@ -0,0 +1,17 @@
+/*
+
+LINK:
+https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/javascript
+
+DESCRIPTION:
+
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+
+*/
+
+// SOLUTION:
+
+const findAverage = (arr) =>
+  arr.reduce((acc, el) => acc + el, 0) / arr.length || 0;
