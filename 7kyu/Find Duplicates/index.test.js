d32b8ea09972ae760600581b3397bb5ecd1a4d47
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2023-06-11 22:02:48 -0300

    Added tests for Find duplicates kata

--- 7kyu/Find Duplicates/index.test.js
+++ 7kyu/Find Duplicates/index.test.js
@@ -12,8 +12,12 @@ test("Array [1,2,'3','3'] should return ['3']", () => {
   expect(duplicates([1, 2, "3", "3"])).toStrictEqual(["3"]);
 });
 
-test("Array ['zut', 'alors', 1, 2, 4, 4, 3, 3, '1', 5, 3, 'zut'] should return ['3']", () => {
+test("Array ['zut', 'alors', 1, 2, 4, 4, 3, 3, '1', 5, 3, 'zut'] should return [4, 3, 'zut']", () => {
   expect(
     duplicates(["zut", "alors", 1, 2, 4, 4, 3, 3, "1", 5, 3, "zut"])
   ).toStrictEqual([4, 3, "zut"]);
+});
+
+test("Array [0,0,'0',2,2,3,3] should return [0,2,3]", () => {
+  expect(duplicates([0, 0, "0", 2, 2, 3, 3])).toStrictEqual([0, 2, 3]);
 });
