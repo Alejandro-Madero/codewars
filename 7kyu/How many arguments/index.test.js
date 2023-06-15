30c2ab5f8154b78676d7f47326445066ad095d8e
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2023-06-14 23:50:39 -0300

    Added tests for 'How many arguments' kata

--- 7kyu/How many arguments/index.test.js
+++ 7kyu/How many arguments/index.test.js
@@ -1,3 +1,21 @@
 const args_count = require(".");
 
-test();
+test("calling args_count with arguments (1,2,3) should return 3", () => {
+  expect(args_count(1, 2, 3)).toBe(3);
+});
+
+test("calling args_count with no arguments should return 0", () => {
+  expect(args_count()).toBe(0);
+});
+
+test("calling args_count with arguments ('10', '9') should return 2", () => {
+  expect(args_count("10", "9")).toBe(2);
+});
+
+test("calling args_count with argument (30) should return 1", () => {
+  expect(args_count(1, 2, 3)).toBe(3);
+});
+
+test("calling args_count with arguments (9,8,7,6,5,4) should return 6", () => {
+  expect(args_count(9, 8, 7, 6, 5, 4)).toBe(6);
+});
